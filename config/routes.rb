d2d3d02f0cf3415dde3a5946a2f7bb2fd1b0db95
Jurgen Feuchter <jfeuchter@gmail.com>
2016-10-20 20:36:36 -0500

    Created Admin side

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :games
+  namespace :admin do
+    resources :games
+    root 'admin/games#index'
+  end
+  root "games#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Creating games..."
+
+#title: string, description: string, url: string, image: string
+Game.create(title: "League of Legends", description: "MOBA game 5v5 or 3v3", url: "http://play.na.leagueoflegends.com/en_US")
+Game.create(title: "Halo 5 Guardians", description: "FPS Future team vrs tema or coop game", url: "https://www.halowaypoint.com/en-us/games/halo-5-guardians")
+
